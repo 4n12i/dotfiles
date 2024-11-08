20b55deb276e6a58eb208f8cf6e417cc3b0d6d30
anrin <an12inakamu12a@gmail.com>
2024-11-08 21:06:33 +0900

    update

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,8 @@
+-- go to next tab
+vim.keymap.set('n', '<C-l>', ':tabnext<CR>')
+-- vim.keymap.set('n', '<C-k>', ':tabnext<CR>')
+
+-- go to previous tab
+vim.keymap.set('n', '<C-h>', ':tabprevious<CR>')
+-- vim.keymap.set('n', '<C-j>', ':tabprevious<CR>')
+
